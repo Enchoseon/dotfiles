07e8fa6280c670a9230cf2da822ba34692edd869
Enchoseon <38841444+Enchoseon@users.noreply.github.com>
2023-04-29 23:16:28 -0700

    add: Pandoc filter to export Vimwiki w/ extension

--- Scripts/vimwiki_converter.sh
+++ Scripts/vimwiki_converter.sh
@@ -3,8 +3,8 @@
 OUTPUT_DIRECTORY=$4 # Absolute path to the output directory
 INPUT_FILE=$5 # Absolute path to the input file
 OUTPUT_FILE=$(basename -- "${INPUT_FILE}")
-OUTPUT_FILE="${OUTPUT_FILE%.*}" # Output filename (without any path attached)
+OUTPUT_FILE="${OUTPUT_FILE%.*}.html" # Output filename (without any path attached)
 # Create directory
 mkdir -p "${OUTPUT_DIRECTORY}"
 # Run Pandoc
-eval pandoc "-i \"${INPUT_FILE}\" --katex --standalone --highlight-style=pygments --wrap=preserve -f markdown+lists_without_preceding_blankline -t html -o \"${OUTPUT_DIRECTORY}/${OUTPUT_FILE}\"" > /dev/null 2>&1
+eval pandoc "-i \"${INPUT_FILE}\" --katex --standalone --highlight-style=pygments --wrap=preserve --lua-filter ~/.config/nvim/vimwiki_linkfix.lua -f markdown+lists_without_preceding_blankline -t html -o \"${OUTPUT_DIRECTORY}/${OUTPUT_FILE}\"" > /dev/null 2>&1
