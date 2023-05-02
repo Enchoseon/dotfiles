8b9b7da0365f3959cb235d2035d64e1e0f4f1f3b
Enchoseon <38841444+Enchoseon@users.noreply.github.com>
2023-05-01 19:19:53 -0700

    cleanup leftover testing code

--- .config/nvim/vimwiki_linkfix.lua
+++ .config/nvim/vimwiki_linkfix.lua
@@ -1,9 +1,5 @@
 -- This simple Pandoc filter appends ".html" to local links. It might screw up local images.
-local path = require "pandoc.path"
-local system = require "pandoc.system"
 function Link(el)
-	local outputDirectory = path.directory(PANDOC_STATE.output_file)
-	local htmlFilePath = path.join({outputDirectory, decodeURI(el.target) .. ".html"})
 	if is_file(el.target) then
 		el.target = el.target .. ".html"
 	end
@@ -15,13 +11,4 @@ function is_file(target)
 		return false
 	end
 	return true
-end
--- Decode URI (https://stackoverflow.com/a/20407113)
-local hex = {}
-for i = 0, 255 do
-	hex[string.format("%02x", i)] = string.char(i)
-	hex[string.format("%02X", i)] = string.char(i)
-end
-function decodeURI(s)
-	return (s:gsub('%%(%x%x)', hex))
 end
