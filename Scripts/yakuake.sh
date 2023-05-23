f92176f1062340437b7d0081fafa8f0a886f3f08
Enchoseon <38841444+Enchoseon@users.noreply.github.com>
2023-05-22 22:09:24 -0700

    add: Scratch terminal to Yakuake autostart
    Splits Yakuake into 3 windows rather than just 2 so that the center
    window can be a scratch terminal for running potato or emerge or
    whatever.

--- Scripts/yakuake.sh
+++ Scripts/yakuake.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-# Run Topydo columns in Yakuake (Note: The "t" alias is defined in ~/.zshrc)
+# Vertical split (2x)
+for run in {1..2}; do qdbus org.kde.yakuake /yakuake/sessions splitTerminalLeftRight "$(qdbus org.kde.yakuake /yakuake/sessions activeTerminalId)"; done
+# Topydo on left, scratch terminal in center, cmus on right
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 0 "t"
-# Split left/right and run Cmus on the right
-qdbus org.kde.yakuake /yakuake/sessions splitTerminalLeftRight "$(qdbus org.kde.yakuake /yakuake/sessions activeTerminalId)"
-qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 1 "cmus && emerge --moo"
-# Grow window to the right (Hack Explanation: Yakuake needs to be visible/toggled in order for tryGrowTerminal* to work)
+qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 1 "emerge --moo"
+qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal 2 "cmus"
+history -d "$(history 3)" # HACK: Remove three previous command from history
+# Grow topydo split to the right (HACK: Yakuake needs to be visible/toggled in order for tryGrowTerminal* to work)
 qdbus org.kde.yakuake /yakuake/window toggleWindowState
-for run in {1..35}; do qdbus org.kde.yakuake /yakuake/sessions tryGrowTerminalRight 0; done
+for run in {1..12}; do qdbus org.kde.yakuake /yakuake/sessions tryGrowTerminalRight 0; done
 qdbus org.kde.yakuake /yakuake/window toggleWindowState
